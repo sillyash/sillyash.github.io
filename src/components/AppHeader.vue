f5f354b15378365d7177762929f2fcad7e8cf87e
Ashley Merienne <ashley.merienne@lmd.ipsl.fr>
2024-09-10 16:55:43 +0200

    LGTM

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="header">
+    <a href="index.html">
+      <img src="/src/assets/mitsuki.jpg"/>
+    </a>
+
+    <div class="navbar">
+      <a
+        v-for="item in items"
+        :key="item.title"
+        :href="item.href"
+        :title="item.title"
+        class="navbar-link"
+        target="_blank"
+      >
+        {{ item.title }}
+      </a>
+    </div>
+  </div>
+
+</template>
+
+<script setup>
+  const items = [
+    {
+      title: 'home',
+      href: 'index.html',
+    },
+    {
+      title: 'about',
+      href: 'index.html#about',
+    },
+    {
+      title: 'projects',
+      href: 'projects.html',
+    },
+    {
+      title: 'contact',
+      href: 'contact.html'
+    },
+    {
+      title: 'github',
+      href: 'https://github.com/sillyash',
+    },
+  ]
+</script>
+
+<style>
+  @import "/src/styles/jetbrains-mono.css";
+  @import "/src/styles/main.css";
+
+  .header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    position: fixed;
+    padding: 10px;
+    width: 100%;
+    height: 70px;
+    background-color: rgb(var(--bg-color-2));
+    color: rgb(var(--text-color-2));
+  }
+
+  .navbar {
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    align-items: center;
+    justify-content: space-around;
+  }
+
+  .navbar-item {
+    color: rgb(var(--text-color-2));
+    text-decoration: overline;
+    transition: 0.3s all;
+  }
+
+  .navbar-item:hover {
+    color: rgb(var(--bg-color-main));
+  }
+
+  a > img {
+    height: 55px;
+    object-fit: contain;
+  }
+</style>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -15,5 +15,20 @@ import { createVuetify } from 'vuetify'
 export default createVuetify({
   theme: {
     defaultTheme: 'dark',
+    myCustomTheme: {
+      dark: false,
+      colors: {
+        'bg-color-main':    '#a0d901',
+        'bg-color-2':       '#000000',
+        'text-color-main':  '#000000',
+        'text-color-2' :    '#ffffff',
+        'text-color-3':     '#4f6b00',
+        'text-color-4':     '#3a6d8c',
+        'text-color-5':     '#542870',
+        'text-color-6':     '#404040',
+        'text-color-footer':'#787878',
+      },
+    },
   },
 })
+
