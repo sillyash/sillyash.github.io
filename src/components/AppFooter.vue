84c52ac75abf8d4821c5230a4af7244f4e982c2c
Ashley <59657132+sillyash@users.noreply.github.com>
2025-09-16 12:11:05 +0200

    Update AppFooter.vue

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -41,19 +41,9 @@
       href: 'https://vuetifyjs.com/',
     },
     {
-      title: 'X',
-      icon: ['M2.04875 3.00002L9.77052 13.3248L1.99998 21.7192H3.74882L10.5519 14.3697L16.0486 21.7192H22L13.8437 10.8137L21.0765 3.00002H19.3277L13.0624 9.76874L8.0001 3.00002H2.04875ZM4.62054 4.28821H7.35461L19.4278 20.4308H16.6937L4.62054 4.28821Z'],
-      href: 'https://x.com/JazzySillyAsh',
-    },
-    {
       title: 'GitHub',
       icon: `mdi-github`,
       href: 'https://github.com/sillyash',
-    },
-    {
-      title: 'LinkedIn',
-      icon: `mdi-linkedin`,
-      href: 'https://linkedin.com/in/ashmerienne',
     },
     {
       title: 'Pronouns Page',
